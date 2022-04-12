64b2a56b719ecf756ea3b86e2d227c6e01f382ff
otaviano <otaviano@DELTAF52>
2022-04-12 17:15:21 -0300

    .

--- VetorMatriz3c2/Program.cs
+++ VetorMatriz3c2/Program.cs
@@ -10,38 +10,59 @@ namespace VetorMatriz3c2
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int[] idade = new int[10];
-            int soma = 0;
-             
-            for (int x =0; x < idade.Length; x++)
-            {
-                idade[x] = random.Next(1,10);
-                soma = soma + idade[x];
-            }
-            // imprimir o conteudo do veto
-            for (int x = 0; x < idade.Length; x++)
-            {
-                Console.WriteLine(idade[x]);
-            }
-            Console.WriteLine("Soma "+soma);
 
-            Console.ReadKey();
+            // Exercicio questao 1
 
-            // Criação de Matriz
-            int[,] notas = new int[6, 2];
+            //int[,] notas = new int[2, 4];
+            // int quantidade = 0;
             // estrutura de repeticao para linha e coluna
-            for (int linha = 0; linha < 6; linha++)
-            {
-                for (int coluna = 0; coluna < 2; coluna++)
-                {
-                    notas[linha, coluna] = random.Next(1, 10);
-                    Console.WriteLine(notas[linha, coluna]+"\t");
-                }
-              
-            }
+            // for (int linha = 0; linha < 2; linha++)
+            // {
+            //     for (int coluna = 0; coluna < 4; coluna++)
+            //      {
+            //         notas[linha, coluna] = random.Next(1, 50);
+            //         Console.WriteLine(notas[linha, coluna]+"\t");
+            //         if (notas[linha, coluna] >= 10 && notas[linha, coluna] <= 15)
+            //          {
+            //             quantidade++;
+            //         }
+            //      }
 
+            //  }
+            //   Console.WriteLine("quantidade"+quantidade);
+            //    Console.ReadKey();
+            //}
+            // Questão 2
+            Random random = new Random();
+            int[,] matriz = new int[3, 2];
+            int[] VetorA = new int[6];
+            int[] VetorB = new int[6];
+            int contPar = 0;
+            int contImpar = 0;
+            //
+            for (int linha = 0; linha < 3; linha++)
+            {
+                for (int coluna=0; coluna < 2; coluna++)
+                {
+                    matriz[linha, coluna] = random.Next(1, 50);
+                    Console.WriteLine(matriz[linha, coluna] + "\t");
+                    // verifica se o numero é par
+                    if (matriz[linha, coluna] % 2 == 0)
+                    {
+                        VetorA[contPar] = matriz[linha, coluna];
+                        contPar++;
+
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                Console.WriteLine("\n");
+            }
             Console.ReadKey();
         }
+
     }
 }
